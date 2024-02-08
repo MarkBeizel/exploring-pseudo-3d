f85cd92ec5a44d315cc78d3486baec2989037019
Mark <markbejzel@gmail.com>
2024-02-08 23:16:11 +0200

    settings.py file added, main.py has been changed.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import pygame
+from settings import *
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
---
+++ settings.py
@@ -0,0 +1,4 @@
+WIDTH = 1200
+HEIGHT = 800
+
+floor_color =  (0, 220, 0)
